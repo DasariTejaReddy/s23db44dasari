662e05ca0ca1964c7484248b7528326b7d902832
DasariTejaReddy <tejareddydasari@gmail.com>
2023-04-14 12:42:47 -0500

    modified Dogs.pug

--- controllers/Dogs.js
+++ controllers/Dogs.js
@@ -69,3 +69,36 @@ exports.Dogs_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 };
+
+// for a specific Costume.
+exports.Dogs_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Dogs.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   // Handle Costume update form on PUT.
+exports.Dogs_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await Dogs.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.Dogname)
+ toUpdate.Dogname = req.body.Dogname;
+ if(req.body.dogcost) toUpdate.dogcost = req.body.dogcost;
+ if(req.body.dogsize) toUpdate.dogsize = req.body.dogsize;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
