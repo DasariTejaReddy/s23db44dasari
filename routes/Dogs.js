9a545ca66a47ca2125c46a5bd443704edc96878c
DasariTejaReddy <tejareddydasari@gmail.com>
2023-04-19 01:09:32 -0500

    added create query

--- routes/Dogs.js
+++ routes/Dogs.js
@@ -4,4 +4,6 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', Dogs_controllers.Dogs_view_all_Page );
 router.get('/detail', Dogs_controllers.Dogs_view_one_Page);
+/* GET create costume page */
+router.get('/create', Dogs_controllers.Dogs_create_Page);
 module.exports = router;
