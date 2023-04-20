6aa4b015c1a90121ac9e6edda71b40b20daf2eb1
DasariTejaReddy <tejareddydasari@gmail.com>
2023-04-19 20:05:14 -0500

    added new delete and update query

--- controllers/Dogs.js
+++ controllers/Dogs.js
@@ -143,4 +143,35 @@ exports.Dogs_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.Dogs_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await Dogs.findById(req.query.id)
+res.render('Dogsupdate', { title: 'Dogs Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+// Handle a delete one view with id from query
+exports.Dogs_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await Dogs.findById(req.query.id)
+res.render('Dogsdelete', { title: 'Dogs Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+
+
     
--- routes/Dogs.js
+++ routes/Dogs.js
@@ -6,4 +6,9 @@ router.get('/', Dogs_controllers.Dogs_view_all_Page );
 router.get('/detail', Dogs_controllers.Dogs_view_one_Page);
 /* GET create costume page */
 router.get('/create', Dogs_controllers.Dogs_create_Page);
+/* GET create update page */
+router.get('/update', Dogs_controllers.Dogs_update_Page);
+/* GET delete costume page */
+router.get('/delete', Dogs_controllers.Dogs_delete_Page);
+
 module.exports = router;
