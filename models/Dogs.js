4731924fa1e56b4df49550e1d25c097620635f05
DasariTejaReddy <tejareddydasari@gmail.com>
2023-04-27 22:14:54 -0500

    added new files

--- models/Dogs.js
+++ models/Dogs.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 const DogSchema = mongoose.Schema({
     Dogname: String,
-    dogsize: String,
-    dogcost: String
-})
+    dogcost: String,
+    dogsize: {
+        type: String,
+        validate: {
+          validator: function (v) {
+            const allowedValues = ["small", "medium", "large"];
+            return allowedValues.includes(v.toLowerCase());
+          },
+          message: "Height must be one of 'small', 'medium', or 'large'",
+        },
+        }
+    })
 module.exports = mongoose.model("Dogs",
 DogSchema)
--- routes/Dogs.js
+++ routes/Dogs.js
@@ -1,13 +1,20 @@
 var express = require('express');
 const Dogs_controllers= require('../controllers/Dogs');
 var router = express.Router();
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET costumes */
 router.get('/', Dogs_controllers.Dogs_view_all_Page );
 router.get('/detail', Dogs_controllers.Dogs_view_one_Page);
 /* GET create costume page */
 router.get('/create', Dogs_controllers.Dogs_create_Page);
 /* GET create update page */
-router.get('/update', Dogs_controllers.Dogs_update_Page);
+router.get('/update',secured, Dogs_controllers.Dogs_update_Page);
 /* GET delete costume page */
 router.get('/delete', Dogs_controllers.Dogs_delete_Page);
 
